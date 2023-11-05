95391e5b4cd6865ea1f59810e9f8f0ea9aa3410b
bitPascals <pascaluzosike61@gmail.com>
2023-11-05 18:00:30 +0100

    Adding my portfolio website.

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
+@app.route("/portfolio-details")
+def portfolio_details():
+    return render_template("portfolio-details.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
